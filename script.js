cc82f78eefd23959648aba67432e95e6a9f902b1
JonaSaavedra <jonathan.saavedra.gonzalez@gmail.com>
2025-05-09 21:38:30 +0200

    Actualización web y fecha límite
    Ahora al llegar a la fecha límite el botón de comprar entradas no redirige a ninguna web. Además, el título avisa de que el período para la compra de entradas ha finalizado, así como el contador.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//--------------------NOTIFICACIONES---------------------
+
 const fieldset = document.querySelector(".form__fieldset");
 
 function notify() {
@@ -5,6 +7,8 @@ function notify() {
 }
 
 console.log(fieldset);
+
+//----------------------TEMPORIZADOR------------------------
 
 //Creo las variables de lastDate, que recoge la fecha final, y now, que recoge la fecha actual.
 const lastDate = new Date("2025/07/06 16:00:00").getTime();
@@ -18,19 +22,32 @@ function updateCountdown() {
   const hours = Math.floor((diff / 1000 / 60 / 60) % 24);
   const minutes = Math.floor((diff / 1000 / 60) % 60);
   const seconds = Math.floor((diff / 1000) % 60);
-  //Declaramos las variables del HTML de días, horas, minutos y segundos
+  //Declaramos las variables del HTML de días, horas, minutos y segundos y asignamos los valores extraídos antes
   const dates = document.querySelectorAll(".date__num");
   dates[0].innerHTML = days;
   dates[1].innerHTML = hours;
   dates[2].innerHTML = minutes;
   dates[3].innerHTML = seconds;
 
-  //   console.log(days)
-  //   console.log(hours)
-  //   console.log(minutes)
-  //   console.log(seconds)
+  let headerInfo = document.querySelector('#info__h1').innerHTML
+
+  if(diff <= 0){
+    dates[0].innerHTML = '00';
+    dates[1].innerHTML = '00';
+    dates[2].innerHTML = '00';
+    dates[3].innerHTML = '00';
+    let headerInfo = document.querySelector('#info__h1').innerHTML = 'Evento finalizado'
+    let infoBuy = document.querySelector('#info__buy')
+    infoBuy.style.backgroundColor = '#F13'
+    infoBuy.removeAttribute('href')
+    infoBuy.style.textDecoration = 'line-through'
+  }
+
+  
 }
 
 const countDown = setInterval(updateCountdown, 1000);
 
 updateCountdown();
+
+
